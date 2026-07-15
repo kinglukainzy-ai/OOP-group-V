7838970a03e06738da2d7172890fc3e766573594
Femi099-source <14poundz@gmail.com>
2026-07-15 10:25:20 -1000

    Update MemberService.java

--- src/org/library/system/service/MemberService.java
+++ src/org/library/system/service/MemberService.java
@@ -1,46 +1,12 @@
 package org.library.system.service;
 
-import java.util.List;
 import org.library.system.model.Member;
-import org.library.system.util.LibraryData;
 
-public class MemberServiceImpl implements MemberService {
+public interface MemberService {
 
-    private final LibraryData libraryData;
+    boolean registerMember(Member member);
 
-    public MemberServiceImpl(LibraryData libraryData) {
-        this.libraryData = libraryData;
-    }
+    Member getMemberById(String memberId);
 
-    @Override
-    public void registerMember(Member member) {
-        libraryData.getLibrary().registerMember(member);
-        System.out.println("Member registered successfully.");
-    }
-
-    @Override
-    public Member getMemberById(String memberId) {
-        List<Member> members = libraryData.getLibrary().getMembers();
-
-        for (Member member : members) {
-            if (member.getMemberId().equals(memberId)) {
-                return member;
-            }
-        }
-
-        return null;
-    }
-
-    @Override
-    public void setSuspensionStatus(String memberId, boolean status) {
-        Member member = getMemberById(memberId);
-
-        if (member == null) {
-            System.out.println("Member not found.");
-            return;
-        }
-
-        member.setSuspended(status);
-        System.out.println("Member suspension status updated.");
-    }
+    void setSuspensionStatus(String memberId, boolean status);
 }
